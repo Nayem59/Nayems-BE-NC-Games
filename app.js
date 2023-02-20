9d125eb30094e523b54a0cc8e056292472f41a6f
Nayem <nayem.alam97@gmail.com>
2023-02-20 15:54:16 +0000

    changed app and model js as per review

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 const { getCategories } = require("./controllers/categories-controllers");
 const { error500Status } = require("./controllers/error-controllers");
 
